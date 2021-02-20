9cd55e3d4383057f596b32382af6515fe668f00f
Oleg Chernyavskyi <ochernyavskyi@gmail.com>
2021-02-20 13:16:30 +0200

    added auth 2.0

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Auth::routes();
 
@@ -31,13 +33,61 @@ Auth::routes();
 Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
-	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
-	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
-	Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
-	 Route::get('map', function () {return view('pages.maps');})->name('map');
-	 Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
-	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+    Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
+    Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
+    Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
+    Route::get('upgrade', function () {
+        return view('pages.upgrade');
+    })->name('upgrade');
+    Route::get('map', function () {
+        return view('pages.maps');
+    })->name('map');
+    Route::get('icons', function () {
+        return view('pages.icons');
+    })->name('icons');
+    Route::get('table-list', function () {
+        return view('pages.tables');
+    })->name('table');
+    Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
+
+Route::get('/github/redirect', function () {
+    return Socialite::driver('github')->redirect();
+})->name('github.redirect');
+
+Route::get('/github/callback', function () {
+    $userGithub = Socialite::driver('github')->user();
+    $user = \App\Models\User::firstOrCreate(
+        [
+            'email' => $userGithub->getEmail()
+        ],
+        [
+            'name' => $userGithub->getName(),
+            'email' => $userGithub->getEmail(),
+            'password' => \Illuminate\Support\Facades\Hash::make(\Illuminate\Support\Str::random(25))
+        ]);
+    Auth::login($user);
+    return redirect()->route('home');
+});
+
+
+Route::get('/google/redirect', function () {
+    return Socialite::driver('google')->redirect();
+})->name('google.redirect');
+
+Route::get('/google/callback', function () {
+    $userGoogle = Socialite::driver('google')->user();
+    $user = \App\Models\User::firstOrCreate(
+        [
+            'email' => $userGoogle->getEmail()
+        ],
+        [
+            'name' => $userGoogle->getName(),
+            'email' => $userGoogle->getEmail(),
+            'password' => \Illuminate\Support\Facades\Hash::make(\Illuminate\Support\Str::random(25))
+        ]);
+    Auth::login($user);
+    return redirect()->route('home');
+});
+
 
